e3b9b17da14b1224f44a247716ac8cc01b1103ec
phantomspectrumcompressor91975 <edmund.spielman@outlook.com>
2024-11-12 16:46:40 -0700

    Create ServerJoined.py

---
+++ ServerJoined.py
@@ -0,0 +1,29 @@
+import re
+from getytchat import ytchat
+
+chat = ytchat("")
+class ServerJoiner():
+    def __init__(self) -> None:
+        self.isFound = False
+        while True:
+            nextC = chat.NextChat()
+            if nextC:
+                author, msg = nextC
+                regex = self.hasRegex(msg.decode())
+                if regex:
+                    self.isFound = True
+                    URL = regex.group()
+                else:
+                    print(f"{author} said: {msg}")
+    
+    def hasRegex(self, string):
+        if self.isFound:
+            print("Not Gonna Try Joining A Private Server Twice")
+        else:
+            regex = re.search(r"https:\/\/www.roblox\.com\/share.code=[a-z0-9-]+&type=Server", string)
+            if regex:
+                self.isFound = True
+                return regex
+            else: return False
+
+s=ServerJoiner()
